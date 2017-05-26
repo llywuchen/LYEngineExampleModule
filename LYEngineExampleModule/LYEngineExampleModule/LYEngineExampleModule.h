e2fe5fc7885ce6ced2bce3c726cafd8f25bada3b
linliyong <linliyong@gomeplus.com>
2017-05-26 15:00:19 +0800

    add public header file

--- LYEngineExampleModule/LYEngineExampleModule/LYEngineExampleModule.h
+++ LYEngineExampleModule/LYEngineExampleModule/LYEngineExampleModule.h
@@ -16,5 +16,6 @@ FOUNDATION_EXPORT const unsigned char LYEngineExampleModuleVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <LYEngineExampleModule/PublicHeader.h>
 
+#import <LYEngineExampleModule/ExampleModule.h>
 
 
